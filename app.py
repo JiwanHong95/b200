f9b47ecf312c9a0fe7208d03bce60189e4dd636b
JiwanHong95 <160742068+JiwanHong95@users.noreply.github.com>
2025-08-12 17:56:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -74,16 +74,16 @@ def show_user_interface():
     st.title("엘리스 클라우드 B200 예약 서비스")
     st.write("원하시는 날짜와 개수를 선택하고 정보를 남겨주세요.")
 
-# 2025-09-22부터 신청 가능
-min_selectable_date = datetime.date(2025, 9, 22)
+    # 2025-09-22부터 신청 가능
+    min_selectable_date = datetime.date(2025, 9, 22)
 
-reservation_dates = st.date_input(
-    "예약 날짜를 선택하세요. (2025년 9월 22일부터 예약 가능)",
-    (min_selectable_date, min_selectable_date + datetime.timedelta(days=1)),
-    min_value=min_selectable_date,
-    max_value=max_date,
-    key="date_selector"
-)
+    reservation_dates = st.date_input(
+        "예약 날짜를 선택하세요. (2025년 9월 22일부터 예약 가능)",
+        (min_selectable_date, min_selectable_date + datetime.timedelta(days=22)),
+        min_value=min_selectable_date,
+        max_value=max_date,
+        key="date_selector"
+    )
 
 
     if isinstance(reservation_dates, tuple) and len(reservation_dates) == 2:
